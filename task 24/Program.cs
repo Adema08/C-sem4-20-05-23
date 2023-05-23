32bba1b0be9fdd6b22911a078006d49f181a85c9
Adema <aatygaeva08@gmail.com>
2023-05-23 23:24:45 +0600

    1.sum 1 n  2.кол-во цифр  3.произв 1 n  4.rand 0 1

---
+++ task 24/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int Sum(int num) {
+    int sum = 0;
+    for(int i = 0; i <= num; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = Sum(num);
+Console.Write($"Сумма чисел от 1 до {num}: {result}");
---
+++ task 26/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int Len(int n) {
+    string s = n.ToString();
+    return s.Length;
+}
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Количество цифр: {Len(Math.Abs(num))}");
